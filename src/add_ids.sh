fb07fe8cd3393084978f56d5cf46a5a7898c9fa9
Risto Kupsala <risto@pandunia.info>
2024-10-15 09:03:36 +0300

    Write English definition file from data/master.tsv

--- src/add_ids.sh
+++ src/add_ids.sh
@@ -1,11 +1,6 @@
 # Get clean id and definition files (erases changes)
 git checkout dict/eng-definition.tsv
-git checkout data/id-concepticon.tsv
 # Run program to append new ids to the id and definition files
-python3 src/create_initial_ids_and_definitions.py
-# Delete appended ids from the workspace file
-#sed -i '/\tready/d' data/id-concepticon-definition.tsv
-#sed -i 's/\t$//' data/id-concepticon-definition.tsv
-# Sort the id and definition files
-gawk -i inplace 'NR<2{print;next}{print|"sort -f"}' data/id-concepticon.tsv
+python3 src/write_definition_file.py
+# Sort the id and definition files starting from line 2 (i.e. ignore the header line)
 gawk -i inplace 'NR<2{print;next}{print|"sort -f"}' dict/eng-definition.tsv
---
+++ src/write_definition_file.py
@@ -0,0 +1,28 @@
+"""Copy final Panlexia concept ids and English definitions to a file.
+
+Initial concept ids for Panlexia are created based on Concepticon and ULD data.
+
+Input: master.tsv:
+    Tab-separated values file for mapping initial Panlexia ids
+    to Concepticon or ULD definition and other ids.
+
+Output: eng-definition.tsv:
+    Data table file with two columns: id and English definition.
+
+CC-BY 2024 Panlexia (https://github.com/barumau/panlexia)
+"""
+import helpers
+
+def create_english_definition_file_from_master():
+    """Writes ready (non-empty) ids and their definitions to a file."""
+
+    definition_writer = helpers.tsv_writer('dict/eng-definition.tsv', 'w')
+    definition_writer.dict.writerow(["id", "Definition"])
+
+    combined = helpers.tsv_reader('data/master.tsv')
+    for row in combined.dict:
+        if row["id"] != "":
+            definition_writer.dict.writerow([row["id"], row["Definition"]])
+
+# Execution begins
+create_english_definition_file_from_master()
